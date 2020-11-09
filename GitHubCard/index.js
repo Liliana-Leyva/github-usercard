91ab03a193280353e748ea58c0f65dda74dbb50b
Liliana Leyva <liliana@Lilianas-MacBook-Pro.local>
2020-11-08 18:11:19 -0600

    Liliana Leyva MVP

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,17 +1,33 @@
+import axios from 'axios'
 /*
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
 
+
+
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
     github info! You will need to understand the structure of this
     data in order to use it to build your component function
 
+    
+
     Skip to STEP 3.
 */
+const getPromise = axios
+.get("https://api.github.com/users/liliana-leyva")
+.then((res) => {
+  console.log(res);
+  const information = cardCreator(res.data);
+  document.querySelector(".cards").appendChild(information);
+})
+.catch((err) => {
+  console.log("Oops",err);
+});
 
+console.log(getPromise);
 /*
   STEP 4: Pass the data received from Github into your function,
     and append the returned markup to the DOM as a child of .cards
@@ -28,7 +44,21 @@
     user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const followersArray = ['tetondan','dustinmyers', 'justsml','luishrd','bigknell'];
+
+followersArray.forEach((i) => {
+  axios
+    .get(`https://api.github.com/users/${i}`)
+    .then((res) => {
+      console.log(res.data);
+      const information = cardCreator(res.data);
+      document.querySelector(".cards").appendChild(information);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+console.log;
 
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
@@ -58,3 +88,62 @@ const followersArray = [];
     luishrd
     bigknell
 */
+
+
+const allCards= getPromise.data;
+console.log(allCards)
+const cardCreator = function(data){
+  /*.card div */
+  const card =  document.createElement('div');
+  const picture = document.createElement('img');
+  /* cardInfo Div  */
+  const cardInfo = document.createElement('div');
+  const h3Name = document.createElement('h3');
+  const userNAme = document.createElement ('p');
+  const userLocation = document.createElement ('p');
+  const userProfile = document.createElement ('p');
+  const userUlr = document.createElement ('a');
+  const userFollowers = document.createElement('p');
+  const userFolowwing = document.createElement('p');
+  const userBio = document.createElement('p');
+  /* cardInfo Div ends here*/
+
+  /*.card div ends here */
+
+
+  card.classList.add("card");
+  card.appendChild(picture);
+  card.appendChild(cardInfo);
+
+  picture.src = data.avatar_url;
+
+  cardInfo.classList.add("card-info");
+
+  cardInfo.appendChild(h3Name);
+  cardInfo.appendChild(userNAme);
+  cardInfo.appendChild(userLocation);
+  cardInfo.appendChild(userUlr);
+  cardInfo.appendChild(userFollowers);
+  cardInfo.appendChild(userFolowwing);
+  cardInfo.appendChild(userBio);
+
+  h3Name.classList.add('name');
+  h3Name.textContent = `${data.name}`
+  
+  userNAme.classList.add("username");
+  userNAme.textContent = `Location: ${data.location}`;
+
+  userProfile.textContent= 'Profile: ';
+  userProfile.appendChild(userUlr);
+
+  userUlr.href = data.html_url;
+  userUlr.text = `${data.html_url}`;
+
+  userFollowers.textContent = `Followers: ${data.followers}`;
+  userFolowwing.textContent = `Following: ${data.following}`;
+
+  userBio.textContent= `Bio: ${data.bio}`;
+
+return card;
+
+}
